8cc6f4055f88a16eb3b328399ad5e28ba9b45b27
unknown <araunasandy@gmail.com>
2026-04-16 19:45:44 +0700

    ye

--- resources/views/backend/layout/main.blade.php
+++ resources/views/backend/layout/main.blade.php
@@ -337,14 +337,15 @@
                         <!-- Dropdown - User Information -->
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                              aria-labelledby="userDropdown">
-                            <a class="dropdown-item" href="{{('dashboard.profile')}}">
+                            <a class="dropdown-item" href="{{route('dashboard.profile')}}">
                                 <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                 Profile
                             </a>
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                setting
+                            <a class="dropdown-item" href="{{ route('dashboard.resetPassword') }}">
+                                <i class="fas fa-key fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Reset Password
                             </a>
+                            <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="#">
                                 <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                                 Activity Log
